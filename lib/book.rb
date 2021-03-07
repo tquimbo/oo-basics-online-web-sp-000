a9328078f54c856f9ce64442cc7d7eb6002aa062
Tim Quimbo <tquimbo1@gmail.com>
2021-03-07 01:25:42 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,19 +16,19 @@ class Book
       @author
     end
 
-    
+
   def page_count=(num)
     @page_count = num
   end
- 
+
   def page_count
     @page_count
   end
- 
+
   def genre=(genre)
     @genre = genre
   end
- 
+
   def genre
     @genre
   end
