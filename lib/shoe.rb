30851f5477cdd6407738384c19325e5ce6fe6c0f
Tim Quimbo <tquimbo1@gmail.com>
2021-03-07 01:31:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def brand
+    @brand
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
